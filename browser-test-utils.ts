6a9a7478f14d93b12b5bfed1e258d57f729606c0
Chao Xu <mgcclx@gmail.com>
2026-06-20 15:12:00 -0700

    Fix browser test helper ESM export

--- browser-test-utils.ts
+++ browser-test-utils.ts
@@ -1,4 +1,11 @@
 /**
  * Top-level entry for @chaoxu/coflat/browser-test-utils.
  */
-export * from "./src/editor/browser-test-utils";
+export {
+  COFLAT_BROWSER_ATTRIBUTES,
+  COFLAT_BROWSER_SELECTORS,
+} from "./src/editor/browser-test-utils.js";
+export type {
+  CoflatBrowserAttribute,
+  CoflatBrowserSelector,
+} from "./src/editor/browser-test-utils.js";
