97801321ff9aefada611bf65c7e254d1285b08d4
Chao Xu <mgcclx@gmail.com>
2026-06-20 18:57:26 -0700

    Expose citation and parity helpers

--- browser-test-utils.ts
+++ browser-test-utils.ts
@@ -2,6 +2,10 @@
  * Top-level entry for @chaoxu/coflat/browser-test-utils.
  */
 export {
+  assertCoflatCodeBlockParity,
+  assertCoflatFootnoteSectionParity,
+  assertCoflatLinkStyleParity,
+  assertCoflatReaderEditorSurfaceParity,
   COFLAT_BROWSER_ATTRIBUTES,
   COFLAT_BROWSER_SELECTORS,
 } from "./src/editor/browser-test-utils.js";
